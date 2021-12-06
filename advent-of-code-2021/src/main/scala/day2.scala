d2a35be2176ffa7cba5ccb1e7e8323821a56caa2
Giancarlo Faranda <giancarlo@dv01.co>
2021-12-06 16:09:51 -0500

    day 2 - part 2

--- advent-of-code-2021/src/main/scala/day2.scala
+++ advent-of-code-2021/src/main/scala/day2.scala
@@ -4,29 +4,52 @@ object day2 {
 
   def main(args: Array[String]): Unit = {
     println(solutionPart1)
+    println(solutionPart2)
   }
 
-  case class Point(x: Int, y: Int) {
+  case class Point(x: Int, y: Int, a: Int = 0) {
     def move(direction: String, length: Int): Point = direction match {
       case "up" => this.copy(y = this.y - length)
       case "down" => this.copy(y = this.y + length)
-      case "forward" => this.copy(this.x + length)
+      case "forward" => this.copy(x = this.x + length)
+    }
+
+    def moveAndAim(direction: String, length: Int): Point = direction match {
+      case "up" => this.copy(a = this.a - length)
+      case "down" => this.copy(a = this.a + length)
+      case "forward" => this.copy(x = this.x + length, y = this.y + (this.a * length))
     }
   }
 
   def solutionPart1: Int = {
     val input = Source.fromResource("2/input").getLines().toSeq
-    val destination = carryOrders(input)
+    val destination = carryOrdersPart1(input)
     destination.x * destination.y
   }
 
-  def carryOrders(instructions: Seq[String], start: Point = Point(0,0)): Point = {
+  def carryOrdersPart1(instructions: Seq[String], start: Point = Point(0,0)): Point = {
     instructions
       .map(_.split(" ") match {
         case Array(x, y) => (x, y.toInt)
       })
       .foldLeft(start) {
         case (point, (direction, length)) => point.move(direction, length)
+      }
+  }
+
+  def solutionPart2: Any = {
+    val input = Source.fromResource("2/input").getLines().toSeq
+    val destination = carryOrdersPart2(input)
+    destination.x * destination.y
+  }
+
+  def carryOrdersPart2(instructions: Seq[String], start: Point = Point(0,0)): Point = {
+    instructions
+      .map(_.split(" ") match {
+        case Array(x, y) => (x, y.toInt)
+      })
+      .foldLeft(start) {
+        case (point, (direction, length)) => point.moveAndAim(direction, length)
       }
   }
 }
--- advent-of-code-2021/src/test/scala/day2Test.scala
+++ advent-of-code-2021/src/test/scala/day2Test.scala
@@ -1,34 +1,34 @@
-import day2.{Point, carryOrders, solutionPart1}
+import day2._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class day2Test extends AnyFlatSpec with Matchers {
 
-  "Translating up 1" should "make y negative" in {
-    Point(0,0).move("up", 1) shouldEqual new Point(0, -1)
+  "Moving up 1" should "make y negative" in {
+    Point(0,0).move("up", 1) shouldEqual Point(0, -1)
   }
 
-  "Translating down 1" should "make y positive" in {
-    Point(0,0).move("down", 1) shouldEqual new Point(0, 1)
+  "Moving down 1" should "make y positive" in {
+    Point(0,0).move("down", 1) shouldEqual Point(0, 1)
   }
 
-  "Translating forward 1" should "make x positive" in {
-    Point(0,0).move("forward", 1) shouldEqual new Point(1, 0)
+  "Moving forward 1" should "make x positive" in {
+    Point(0,0).move("forward", 1) shouldEqual Point(1, 0)
   }
 
-  "Moving up" should "decrease y" in {
-    carryOrders(Seq("up 1")) shouldEqual new Point(0, -1)
+  "Ordering up" should "decrease y" in {
+    carryOrdersPart1(Seq("up 1")) shouldEqual Point(0, -1)
   }
 
-  "Moving down" should "increase y" in {
-    carryOrders(Seq("down 1")) shouldEqual new Point(0, 1)
+  "Ordering down" should "increase y" in {
+    carryOrdersPart1(Seq("down 1")) shouldEqual Point(0, 1)
   }
 
-  "Moving forward" should "increase x" in {
-    carryOrders(Seq("forward 1")) shouldEqual new Point(1, 0)
+  "Ordering forward" should "increase x" in {
+    carryOrdersPart1(Seq("forward 1")) shouldEqual Point(1, 0)
   }
 
-  "Movement example" should "end with x=15,y=10" in {
+  "Part 1 example" should "end with x=15,y=10" in {
     val instructions = Seq(
       "forward 5",
       "down 5",
@@ -37,10 +37,26 @@ class day2Test extends AnyFlatSpec with Matchers {
       "down 8",
       "forward 2"
     )
-    carryOrders(instructions) shouldEqual new Point(15, 10)
+    carryOrdersPart1(instructions) shouldEqual Point(15, 10)
   }
 
-  "Solution" should "multiply x=1890,y=1172 = 2215080" in {
+  "Part 1 solution" should "multiply x=1890,y=1172 = 2215080" in {
     solutionPart1 shouldEqual 2215080
+  }
+
+  "Part 2 example" should "end with x=15,y=60" in {
+    val instructions = Seq(
+      "forward 5",
+      "down 5",
+      "forward 8",
+      "up 3",
+      "down 8",
+      "forward 2"
+    )
+    carryOrdersPart2(instructions) shouldEqual Point(15, 60, 10)
+  }
+
+  "Part 2 solution" should "multiply x=1890,y=986622 = 1864715580" in {
+    solutionPart2 shouldEqual 1864715580
   }
 }
